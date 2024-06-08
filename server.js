a9bd13609aaf86343011966435f5ffc4b5d171bf
Denis Adamčík <118216913+DenisAdamcik@users.noreply.github.com>
2024-06-08 13:35:39 +0200

    Update server.js

--- server.js
+++ server.js
@@ -22,18 +22,32 @@ app.get('/scoreboard.json', async (req, res) => {
 });
 
 app.post('/save-score', async (req, res) => {
+    // Destrukturuje požadované údaje z těla požadavku
     const { playerName, balance, maxScore } = req.body;
+
+    // Ověří, zda jsou všechna potřebná data v požadavku přítomna
     if (!playerName || balance == null || maxScore == null) {
         return res.status(400).json({ error: 'Invalid request data' });
     }
+
     try {
+        // Definuje cestu k souboru scoreboard.json
         const scoreboardFilePath = __dirname + '/public/scoreboard.json';
+
+        // Čte obsah scoreboard.json a převádí jej do JavaScript objektu
         const data = await fs.readFile(scoreboardFilePath, 'utf8');
         const scoreboard = JSON.parse(data);
+
+        // Přidává nové skóre do scoreboardu
         scoreboard.push({ playerName, balance, maxScore });
+
+        // Zapisuje aktualizovaný scoreboard zpět do scoreboard.json
         await fs.writeFile(scoreboardFilePath, JSON.stringify(scoreboard, null, 2));
+
+        // Odesílá úspěšnou odpověď
         res.status(200).json({ message: 'Score saved successfully' });
     } catch (error) {
+        // Zachycuje chyby
         console.error('Error saving score:', error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
